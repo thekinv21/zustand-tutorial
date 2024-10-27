58cadac134de3b68138fd1219f6efd7c885c441c
Vadim Kiniabaev <thekinv21@gmail.com>
2024-10-27 22:20:27 +0300

    feat:[installation][Vadim]: create home page

---
+++ src/home/HomePage.tsx
@@ -0,0 +1,7 @@
+export function HomePage() {
+	return (
+		<section className='flex h-screen items-center justify-center'>
+			<h1 className='text-2xl'>Zustand Tutorial Examples!</h1>
+		</section>
+	)
+}
