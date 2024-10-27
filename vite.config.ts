91b83ebbad961cb2215a0800534e5be6233927c4
Vadim Kiniabaev <thekinv21@gmail.com>
2024-10-27 22:13:52 +0300

    feat:[installation][vadim]: fix path aliaces and also added base url for config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				silenceDeprecations: ['legacy-js-api']
+			}
+		}
+	},
+
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src')
+		}
+	}
 })
