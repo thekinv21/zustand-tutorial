3120e267956ecd321b30af8bf4dc7725da58ecc3
Vadim Kiniabaev <thekinv21@gmail.com>
2024-10-27 23:47:31 +0300

    feat:[tutorial][VADİM]: create middleware on zustand

--- src/home/HomePage.tsx
+++ src/home/HomePage.tsx
@@ -1,10 +1,10 @@
-import { BearCounter } from './BearCounter'
+import { BearCountLocaleStorage } from './BearCountLocaleStorage'
 import styles from './HomePage.module.scss'
 
 export function HomePage() {
 	return (
 		<section className={styles.container}>
-			<BearCounter />
+			<BearCountLocaleStorage />
 		</section>
 	)
 }
